8fbd7d9cd7c0dade744e5cc48bb8fac7d40b2851
JesusDFernandez <78115281+JesusDFernandez@users.noreply.github.com>
2023-11-10 15:04:53 -0400

    middlewares auth validator

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
+
+export const auth = (req, res, next) => {
+  try {
+
+    const token = req.headers.authorization;
+
+    if (!token)
+      return res
+        .status(401)
+        .json({ message: "Sin token, autorización denegada" });
+
+    jwt.verify(token, TOKEN_SECRET, (error, user) => {
+      if (error) {
+        return res.status(401).json({ message: "El token no es válido" });
+      }
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
